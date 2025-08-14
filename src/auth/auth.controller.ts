89107c8377ab48f07cdfbfa925e9a7efb6b2cfc5
Albin <alvinfer67@gmail.com>
2025-08-13 23:12:58 -0600

    feat(auth): use http-only cookies for authentication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,18 @@
-import { Controller, Post, Body, UnauthorizedException, Get, UseGuards, Req } from '@nestjs/common';
+import { Controller, Post, Body, UnauthorizedException, Get, UseGuards, Req, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { AuthGuard } from '@nestjs/passport';
 import { Roles } from './decorators/roles.decorator';
 import { RoleName } from '../entities/Role.entity';
 import { RolesGuard } from './guards/roles.guard';
+import express from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
-  async login(@Body() loginDto: LoginDto) {
+  async login(@Body() loginDto: LoginDto, @Res({ passthrough: true }) res: express.Response) {
     const user = await this.authService.validateUser(
       loginDto.username,
       loginDto.password,
@@ -19,7 +20,15 @@ export class AuthController {
     if (!user) {
       throw new UnauthorizedException();
     }
-    return this.authService.login(user);
+    const { access_token } = await this.authService.login(user);
+    res.cookie('jwt', access_token, { httpOnly: true });
+    return { message: 'Login successful' };
+  }
+
+  @Post('logout')
+  async logout(@Res({ passthrough: true }) res: express.Response) {
+    res.clearCookie('jwt');
+    return { message: 'Logout successful' };
   }
 
   @Get('profile')
--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,9 +1,18 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Strategy } from 'passport-jwt';
 import { AuthService } from '../auth.service';
 import { LoginPayload } from '../interfaces/login-payload.interface';
 import { ConfigService } from '@nestjs/config';
+import { Request } from 'express';
+
+const cookieExtractor = (req: Request): string | null => {
+  let token = null;
+  if (req && req.cookies) {
+    token = req.cookies['jwt'];
+  }
+  return token;
+};
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -12,7 +21,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     private readonly configService: ConfigService,
   ) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: cookieExtractor,
       secretOrKey: configService.get<string>('JWT_SECRET') as string,
     });
   }
